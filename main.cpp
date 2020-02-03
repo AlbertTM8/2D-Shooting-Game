1dea41a9b74c9cbe8834e9c801e75025c10136e9
eencae <el18ajst@leeds.ac.uk>
2020-02-03 14:26:29 +0000

    ELEC2645 starter project

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/* 
+ELEC2645 Embedded Systems Project
+School of Electronic & Electrical Engineering
+University of Leeds
+2019/20
+
+Name:
+Username:
+Student ID Number:
+Date:
+*/
+
+// includes
+#include "mbed.h"
+#include "Gamepad.h"
+#include "N5110.h"
+
+
+// objects
+Gamepad pad;
+N5110 lcd;
+
+int main()
+{
+    
+}
+
