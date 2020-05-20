a31940d148787bc820f00b2ca49372f6f4e8ef82
Albutt <tanmulliganalbert@gmail.com>
2020-05-20 21:39:09 +0000

    Enemy Class Finished;

---
+++ Enemy/Enemy.h
@@ -0,0 +1,23 @@
+#include "mbed.h"
+#include "N5110.h"
+#include "Gamepad.h"
+
+
+class Enemy
+{
+public:
+
+    Enemy();
+    ~Enemy();
+    void init();
+    void draw(N5110 &lcd);
+    void update(int player_x, int player_y);
+    int get_x();
+    int get_y();
+
+private:
+
+    int _x;
+    int _y;
+    int _speed;
+};
