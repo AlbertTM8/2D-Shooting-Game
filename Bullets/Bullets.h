520b429361c8351a50a3b509b4d02a9e48cca2d2
Albutt <Albutt@localhost>
2020-05-26 23:34:57 +0000

    Testing Documents

--- Bullets/Bullets.h
+++ Bullets/Bullets.h
@@ -1,6 +1,7 @@
 /** Bullets Class
+@brief  - Defines instances of bullets for a 2D shooter game
+
 @author Albert Tan Muligan, Student at University of Leeds
-@brief Controls the Character in a shooter game 
 @date May 2020
 
 
@@ -13,16 +14,22 @@ class Bullets
 {
 
 public:
+    /** 
+  * @brief Constructor that sets inital x and y
+  * @param x and y  @details x and y in pixels (max x = 84, y = 48)
+  */
     Bullets(int ex, int wy, int d);
     ~Bullets();
     void draw(N5110 &lcd);
     void update();
+    ///Y value accessor
     int get_y();
+    ///X value accessor
     int get_x();
+    ///"Dead" function puts the x and y at 1000 so it's off-screen.
     void dead();
 private:
     int _x;
     int _y;
     int _dir;
 };
-#endif
--- Enemy/Enemy.h
+++ Enemy/Enemy.h
@@ -2,18 +2,37 @@
 #include "N5110.h"
 #include "Gamepad.h"
 
+/** Enemy Class
+@brief  - Defines instances of Enemies for a 2D shooter game
+
+@author Albert Tan Muligan, Student at University of Leeds
+@date May 2020
+
+
+*/ 
 
 class Enemy
 {
 public:
+    /** 
+  * @brief Constructor that sets inital x and y with random seet
+  * @param x and y  @details x and y in pixels (places enemy somewhere on the edge of a 84x48 board)
+  */
     Enemy(int seed);
     ~Enemy();
     void draw(N5110 &lcd);
     void update(int player_x, int player_y);
+    ///X value accessor
     int get_x();
+    ///Y value accessor
     int get_y();
+     /** 
+  * @brief Reset enemy to edge with random seed, and creates an animation on death
+  * @param seed, lcd  @details integer seed and pointer to lcd
+  */
     void reset(int seed, N5110 &lcd);
 private:
+    ///small animation on death
     void death_animation(N5110 &lcd);
     int _x;
     int _y;
