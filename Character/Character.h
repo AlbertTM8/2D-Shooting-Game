abfb6d175c0ab19488e7bfa5f53ab14310d015cc
Albutt <tanmulliganalbert@gmail.com>
2020-05-26 23:34:57 +0000

    Testing Documents

--- Character/Character.h
+++ Character/Character.h
@@ -2,7 +2,7 @@
 #include "N5110.h"
 #include "Gamepad.h"
 /** Character Class
-@brief  - Controls the Character in a shooter game 
+@brief  - Controls the Character in a 2D shooter game 
 
 @author Albert Tan Muligan, Student at University of Leeds
 @date May 2020
